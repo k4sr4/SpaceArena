7ad9460eec36a262131d90d7c9c4e8a2ecf25ebe
k4sr4 <kasra.j012@gmail.com>
2018-11-03 03:34:04 -0700

    Added portals

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -6,7 +6,16 @@ public class CharacterController : MonoBehaviour {
 
     public float speed;             
 
-    private Rigidbody2D rb2d;       
+    private Rigidbody2D rb2d;
+
+    private bool facingRight = true;
+    private bool facingUp = true;
+
+    private float moveHorizontal;
+    private float moveVertical;
+
+    private float rotateHorizontal;
+    private float rotateVertical;
 
     void Start()
     {
@@ -15,26 +24,15 @@ public class CharacterController : MonoBehaviour {
 
     void FixedUpdate()
     {
-        float moveHorizontal;
-        float moveVertical;
-
-        float rotateHorizontal;
-        float mrotateVertical;
-
-        //if (!Input.anyKey)
-        //{
-        //    rb2d.velocity = new Vector2(0f, 0f);
-        //    moveHorizontal = 0f;
-        //    moveVertical = 0f;
-        //}
-
         if (Input.GetAxis("Horizontal") > 0)
         {
             moveHorizontal = 1f;
+            facingRight = true;
         }
         else if (Input.GetAxis("Horizontal") < 0)
         {
             moveHorizontal = -1f;
+            facingRight = false;
         }
         else
         {
@@ -44,10 +42,12 @@ public class CharacterController : MonoBehaviour {
         if (Input.GetAxis("Vertical") > 0)
         {
             moveVertical = 1f;
+            facingUp = true;
         }
         else if (Input.GetAxis("Vertical") < 0)
         {
             moveVertical = -1f;
+            facingUp = false;
         }
         else
         {
@@ -65,5 +65,54 @@ public class CharacterController : MonoBehaviour {
 
         //rb2d.AddForce(movement * speed);
         rb2d.velocity = movement * speed;
+    }
+
+    void FlipHor()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    void FlipVer()
+    {
+        facingUp = !facingUp;
+        Vector3 theScale = transform.localScale;
+        theScale.y *= -1;
+        transform.localScale = theScale;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Portal")
+        {
+            GameObject sourcePortal = collision.gameObject;
+
+            if (sourcePortal.GetComponent<PortalScript>().active)
+            {
+                List<GameObject> portals = GameObject.FindObjectOfType<GameController>().portals;
+                List<GameObject> tempPortals = new List<GameObject>(portals);
+                tempPortals.Remove(sourcePortal);
+                int index = Random.Range(0, tempPortals.Count);
+                GameObject destinationPortal = tempPortals[index];
+                PortalScript destScript = destinationPortal.GetComponent<PortalScript>();
+                destScript.active = false;
+                transform.position = destinationPortal.transform.position;
+
+                if ((destScript.exitRight && moveHorizontal == -1f)
+                    || (!destScript.exitRight && moveHorizontal == 1f))
+                {
+                    FlipHor();
+                }
+
+                if ((destScript.exitUp && moveVertical == -1f)
+                    || (!destScript.exitUp && moveVertical == 1f))
+                {
+                    FlipVer();
+                }
+            }
+        }
+
     }
 }
---
+++ Assets/Scripts/PortalScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour {
+
+    public bool active = true;
+
+    public bool exitRight;
+    public bool exitUp;
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            active = true;
+        }
+    }
+}
