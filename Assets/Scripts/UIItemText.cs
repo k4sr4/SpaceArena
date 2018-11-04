c6b5db69c983c5d67203a6d96bdb27137d3d86b6
k4sr4 <kasra.j012@gmail.com>
2018-11-04 15:15:09 -0800

    Turned covers

---
+++ Assets/Scripts/UIItemText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIItemText : MonoBehaviour {
+
+    public Sprite[] itemTexts;
+	
+	public void Activate (int index)
+    {
+        GetComponent<Image>().enabled = true;
+        GetComponent<Image>().sprite = itemTexts[index];
+
+    }
+
+    IEnumerator WaitAndDisable()
+    {
+        yield return new WaitForSeconds(.5f);
+        GetComponent<Image>().enabled = false;
+    }
+}
